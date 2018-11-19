b18ca8c51ed52b924740ba2692d80b7bfaf4b5f3
Han Wang <wang_han@iapcm.ac.cn>
2018-11-19 09:13:49 +0800

    add bcc.py

---
+++ data/tools/bcc.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def numb_atoms () :
+    return 2
+
+def gen_box () :    
+    return np.eye(3)
+
+def poscar_unit (latt) :
+    box = gen_box()
+    ret  = ""
+    ret += "FCC : a = %f \n" % latt
+    ret += "%.16f\n" % (latt)
+    ret += "%.16f %.16f %.16f\n" % (box[0][0], box[0][1], box[0][2])
+    ret += "%.16f %.16f %.16f\n" % (box[1][0], box[1][1], box[1][2])
+    ret += "%.16f %.16f %.16f\n" % (box[2][0], box[2][1], box[2][2])
+    ret += "Type\n"
+    ret += "%d\n" % numb_atoms()
+    ret += "Direct\n"
+    ret += "%.16f %.16f %.16f\n" % (0.0, 0.0, 0.0)
+    ret += "%.16f %.16f %.16f\n" % (0.5, 0.5, 0.5)
+    return ret
+
