299f1130be0b00d0646b5553fa41f1e671bedc07
Han Wang <wang_han@iapcm.ac.cn>
2019-06-07 21:36:17 +0800

    add test_parse_cur_job.py and test_make_md.py

---
+++ generator/tests/test_make_md.py
@@ -0,0 +1,154 @@
+import os,json,glob,shutil,filecmp
+import dpdata
+import numpy as np
+import unittest
+
+from context import make_model_devi
+from context import parse_cur_job
+from context import param_file
+from context import machine_file
+
+def _make_fake_models(idx, numb_models) :
+    train_dir = os.path.join('iter.%06d' % idx, 
+                             '00.train')
+    os.makedirs(train_dir, exist_ok = True)
+    pwd = os.getcwd()
+    os.chdir(train_dir)
+    for ii in range(numb_models) :
+        os.makedirs('%03d' % ii, exist_ok = True)
+        with open(os.path.join('%03d' % ii, 'forzen_model.pb'), 'w') as fp:
+            fp.write(str(ii))
+        if not os.path.isfile('graph.%03d.pb' % ii) :
+            os.symlink(os.path.join('%03d' % ii, 'forzen_model.pb'), 
+                       'graph.%03d.pb' % ii)
+    os.chdir(pwd)
+
+def _test_cell(testCase, system_1, system_2, places = 5):
+    testCase.assertEqual(system_1.get_nframes(),
+                         system_2.get_nframes())        
+    for ff in range(system_1.get_nframes()) :
+        for ii in range(3) :
+            for jj in range(3) :
+                testCase.assertAlmostEqual(system_1.data['cells'][ff][ii][jj], 
+                                           system_2.data['cells'][ff][ii][jj], 
+                                           places = places,
+                                           msg = 'cell[%d][%d][%d] failed' % (ff,ii,jj))
+
+def _test_coord(testCase, system_1, system_2, places = 5): 
+    testCase.assertEqual(system_1.get_nframes(),
+                         system_2.get_nframes())
+    # think about direct coord
+    tmp_cell = system_1.data['cells']
+    tmp_cell = np.reshape(tmp_cell, [-1, 3])
+    tmp_cell_norm = np.reshape(np.linalg.norm(tmp_cell, axis = 1), [-1, 3])
+    for ff in range(system_1.get_nframes()) :
+        for ii in range(sum(system_1.data['atom_numbs'])) :
+            for jj in range(3) :
+                testCase.assertAlmostEqual(system_1.data['coords'][ff][ii][jj] / tmp_cell_norm[ff][jj], 
+                                           system_2.data['coords'][ff][ii][jj] / tmp_cell_norm[ff][jj], 
+                                           places = places,
+                                           msg = 'coord[%d][%d][%d] failed' % (ff,ii,jj))
+
+def _check_confs(testCase, idx, jdata) :
+    md_dir = os.path.join('iter.%06d' % idx, 
+                          '01.model_devi')
+    tasks = glob.glob(os.path.join(md_dir, 'task.*'))
+    tasks.sort()
+    cur_job = jdata['model_devi_jobs'][idx]
+    sys_idx = cur_job['sys_idx']
+    sys_configs = jdata['sys_configs']
+    poscars = []
+    for ii in sys_idx :
+        sys_poscars = []
+        for ss in sys_configs[ii]:
+            tmp_poscars = glob.glob(ss)
+            sys_poscars += tmp_poscars
+        sys_poscars.sort()
+        poscars.append(sys_poscars)
+    for ii in tasks :
+        conf_file = os.path.join(ii, 'conf.lmp')
+        l_conf_file = os.path.basename(os.readlink(conf_file))
+        poscar_file = poscars[int(l_conf_file.split('.')[0])][int(l_conf_file.split('.')[1])]
+        sys_0 = dpdata.System(conf_file)
+        sys_1 = dpdata.System(poscar_file)
+        _test_cell(testCase, sys_0, sys_1)
+        _test_coord(testCase, sys_0, sys_1)
+        
+
+def _check_pb(testCase, idx) :
+    md_dir = os.path.join('iter.%06d' % idx, 
+                          '01.model_devi')
+    tr_dir = os.path.join('iter.%06d' % idx, 
+                          '00.train')
+    md_pb = glob.glob(os.path.join(md_dir, 'grapb*pb'))
+    tr_pb = glob.glob(os.path.join(tr_dir, 'grapb*pb'))
+    md_pb.sort()
+    tr_pb.sort()
+    for ii,jj in zip(md_pb, tr_pb) :
+        testCase.assertTrue(filecmp.cmp(ii,jj))
+
+        
+def _check_traj_dir(testCase, idx) :
+    md_dir = os.path.join('iter.%06d' % idx, 
+                          '01.model_devi')
+    tasks = glob.glob(os.path.join(md_dir, 'task.*'))
+    tasks.sort()
+    for ii in tasks:
+        testCase.assertTrue(os.path.isdir(os.path.join(ii, 'traj')))
+
+
+def _get_lammps_pt(lmp_input) :
+    with open(lmp_input) as fp: 
+        for ii in fp:
+            if 'variable' in ii and 'TEMP' in ii :
+                lt = float(ii.split()[3])
+            if 'variable' in ii and 'PRES' in ii :
+                lp = float(ii.split()[3])
+    return lt,lp
+
+def _check_pt(testCase, idx, jdata) :
+    md_dir = os.path.join('iter.%06d' % idx, 
+                          '01.model_devi')
+    tasks = glob.glob(os.path.join(md_dir, 'task.*'))
+    tasks.sort()
+    cur_job = jdata['model_devi_jobs'][idx]
+    ensemble, nsteps, trj_freq, temps, press, pka_e, dt = parse_cur_job(cur_job)
+    testCase.assertTrue(ensemble, 'npt')
+    # get poscars
+    sys_idx = cur_job['sys_idx']
+    sys_configs = jdata['sys_configs']
+    poscars = []
+    for ii in sys_idx :
+        sys_poscars = []
+        for ss in sys_configs[ii]:
+            tmp_poscars = glob.glob(ss)
+            sys_poscars += tmp_poscars
+        sys_poscars.sort()
+        poscars.append(sys_poscars)
+    for sidx,ii in enumerate(poscars) :
+        count = 0
+        for ss in ii:
+            for tt in temps:
+                for pp in press:
+                    task_dir = os.path.join(md_dir, 'task.%03d.%06d' % (sidx, count))
+                    lt, lp = _get_lammps_pt(os.path.join(task_dir, 'input.lammps'))
+                    testCase.assertAlmostEqual(lt, tt)
+                    testCase.assertAlmostEqual(lp, pp)
+                    count += 1
+                    
+
+class TestMakeModelDevi(unittest.TestCase):
+    def test_make_model_devi (self) :        
+        with open (param_file, 'r') as fp :
+            jdata = json.load (fp)
+        with open (machine_file, 'r') as fp:
+            mdata = json.load (fp)
+        _make_fake_models(0, jdata['numb_models'])
+        make_model_devi(0, jdata, mdata)
+        _check_confs(self, 0, jdata)
+        _check_traj_dir(self, 0)
+        _check_pt(self, 0, jdata)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ generator/tests/test_parse_cur_job.py
@@ -0,0 +1,64 @@
+import numpy as np
+import unittest
+
+from context import parse_cur_job
+from context import param_file
+from context import machine_file
+
+class TestParseCurJob(unittest.TestCase):
+    def test_npt (self) :        
+        ens = 'npt'
+        ts = [100,200]
+        ps = [1e5,1e6,1e7]
+        ns = 1000
+        tf = 10
+        cur_job = {}
+        cur_job['ens'] = ens
+        cur_job['Ts'] = ts
+        cur_job['Ps'] = ps
+        cur_job['nsteps'] = ns
+        cur_job['t_freq'] = tf
+        res = parse_cur_job(cur_job)
+        for ii,jj in zip(res, [ens, ns, tf, ts, ps, None, None]) :
+            self.assertEqual(ii,jj)
+
+    def test_nvt (self) :        
+        ens = 'nvt'
+        ts = [100,200]
+        ps = [1e5,1e6,1e7]
+        ns = 1000
+        tf = 10
+        cur_job = {}
+        cur_job['ens'] = ens
+        cur_job['Ts'] = ts
+        cur_job['Ps'] = ps
+        cur_job['nsteps'] = ns
+        cur_job['t_freq'] = tf
+        res = parse_cur_job(cur_job)
+        for ii,jj in zip(res, [ens, ns, tf, ts, [-1], None, None]) :
+            self.assertEqual(ii,jj)
+
+    def test_pka (self) :        
+        ens = 'nvt'
+        ts = [100,200]
+        ps = [1e5,1e6,1e7]
+        ns = 1000
+        tf = 10
+        pka = [10, 20, 30]
+        dt = 0.001
+        cur_job = {}
+        cur_job['ens'] = ens
+        cur_job['Ts'] = ts
+        cur_job['Ps'] = ps
+        cur_job['nsteps'] = ns
+        cur_job['t_freq'] = tf
+        cur_job['pka_e'] = pka
+        cur_job['dt'] = dt
+        res = parse_cur_job(cur_job)
+        for ii,jj in zip(res, [ens, ns, tf, ts, [-1], pka, dt]) :
+            self.assertEqual(ii,jj)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
