6b4db8895fc9068f60c64570a4937e25f10c37b5
Han Wang <wang_han@iapcm.ac.cn>
2019-06-07 18:09:27 +0800

    add test for make_train

---
+++ generator/tests/context.py
@@ -0,0 +1,7 @@
+import sys,os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from run import *
+
+param_file = 'param-mg-vasp.json'
+machine_file = 'machine-local.json'
+
