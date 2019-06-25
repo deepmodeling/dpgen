b5b12eab2212c456a93424e2a0a379041f914cbd
Han Wang <wang_han@iapcm.ac.cn>
2019-06-25 19:28:48 +0800

    set generators test context

--- tests/generator/context.py
+++ tests/generator/context.py
@@ -1,5 +1,5 @@
 import sys,os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dpgen.generator.run import *
 
 param_file = 'param-mg-vasp.json'
