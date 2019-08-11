2f8a6d6675975397bdde3a59f2538b34851320f4
Han Wang <wang_han@iapcm.ac.cn>
2019-08-11 12:45:30 +0800

    remove the upper bound of numpy version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     with open(readme_file) as f:
         readme = f.read()
 
-install_requires=['numpy>=1.14.3,<1.17', 'dpdata>=0.1.2', 'pymatgen', 'ase', 'monty>2.0.0', 'paramiko', 'custodian']
+install_requires=['numpy>=1.14.3', 'dpdata>=0.1.2', 'pymatgen', 'ase', 'monty>2.0.0', 'paramiko', 'custodian']
 
 setuptools.setup(
     name=NAME,
