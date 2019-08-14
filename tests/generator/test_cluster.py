69bf01e8d3af60156a7dd29c5d37109123b9d309
Jinzhe Zeng <njzjz@qq.com>
2019-08-14 18:10:45 +0800

    add test_cluster unittest

---
+++ tests/generator/test_cluster.py
@@ -0,0 +1,34 @@
+import dpdata
+import numpy as np
+import unittest
+
+from context import take_cluster
+from comp_sys import CompSys
+
+
+class Test_take_cluster(unittest.TestCase, CompSys):
+    def setUp (self) :
+        type_map = ['C', 'H']
+        jdata={
+            "cluster_cutoff": 3.5
+        }
+        self.system_1 = take_cluster("cluster/14400.lammpstrj", type_map, 1125, jdata)
+        self.system_2 = dpdata.LabeledSystem("cluster/input0_old.log", fmt="gaussian/log")
+        self.system_2.data['cells'] = self.system_1['cells']
+        self.places=0
+
+
+class Test_take_cluster_minify(unittest.TestCase, CompSys):
+    def setUp (self) :
+        type_map = ['C', 'H']
+        jdata={
+            "cluster_cutoff": 3.5,
+            "cluster_minify": True
+        }
+        self.system_1 = take_cluster("cluster/14400.lammpstrj", type_map, 1125, jdata)
+        self.system_2 = dpdata.LabeledSystem("cluster/input0_new.log", fmt="gaussian/log")
+        self.system_2.data['cells'] = self.system_1['cells']
+        self.places=0
+
+if __name__ == '__main__':
+    unittest.main()
