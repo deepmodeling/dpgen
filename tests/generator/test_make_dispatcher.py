e9023ff5f497169ed9338fea5378a30024d6de31
Han Wang <wang_han@iapcm.ac.cn>
2019-09-20 14:20:39 +0800

    add unittest for make_dispatcher

---
+++ tests/generator/test_make_dispatcher.py
@@ -0,0 +1,41 @@
+import unittest
+
+from context import make_dispatcher
+
+class TestDispatcher(unittest.TestCase):
+    # def test_ssh_slurm(self):
+    #     dis = make_dispatcher({
+    #         'batch':    'slurm',
+    #         'hostname': 'localhost',
+    #         'username': 'wanghan',
+    #         'port':     22,
+    #         'work_path': '.',
+    #     })
+    #     self.assertEqual(dis.context.__name__, 'SSHContext')
+    #     self.assertEqual(dis.batch.__name__, 'Slurm')
+
+    def test_local_slurm(self):
+        dis = make_dispatcher({
+            'batch':    'slurm',
+            'work_path': '.',
+        })
+        self.assertEqual(dis.context.__name__, 'LocalContext')
+        self.assertEqual(dis.batch.__name__, 'Slurm')
+
+    def test_lazy_local_slurm(self):
+        dis = make_dispatcher({
+            'batch':    'slurm',
+            'lazy_local': True,
+            'work_path': '.',
+        })
+        self.assertEqual(dis.context.__name__, 'LazyLocalContext')
+        self.assertEqual(dis.batch.__name__, 'Slurm')
+
+    def test_dep_lazy_local_slurm(self):
+        dis = make_dispatcher({
+            'machine_type':    'slurm',
+            'lazy_local': True,
+            'work_path': '.',
+        })
+        self.assertEqual(dis.context.__name__, 'LazyLocalContext')
+        self.assertEqual(dis.batch.__name__, 'Slurm')
