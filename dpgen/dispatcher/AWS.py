9e1e31681d4a31ea595565eeda0e7f4a6342a0c0
felix5572 <yuanfengbo888@pku.edu.cn>
2019-10-06 14:02:19 +0000

    AWS support restrat

--- dpgen/dispatcher/AWS.py
+++ dpgen/dispatcher/AWS.py
@@ -17,33 +17,6 @@ class AWS(Batch):
     _jobQueue = ""
     _query_next_allow_time = datetime.now().timestamp()
 
-    @classmethod
-    def AWS_check_status(cls, caller_instance=None):
-        """
-        to aviod query jobStatus too often, set a time interval
-        query_dict example:
-        {job_id: JobStatus}
-
-        {'40fb24b2-d0ca-4443-8e3a-c0906ea03622': <JobStatus.running: 3>,
-         '41bda50c-0a23-4372-806c-87d16a680d85': <JobStatus.waiting: 2>}
-
-           
-        """
-        if datetime.now().timestamp() > cls._query_next_allow_time:
-            cls._query_next_allow_time=datetime.now().timestamp()+cls._query_time_interval
-            query_dict ={}
-            for status in ['SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING', 'RUNNING','SUCCEEDED', 'FAILED']:
-                status_response = cls.batch.list_jobs(jobQueue=cls._jobQueue, jobStatus=status, maxResults=cls._query_max_results)
-                status_list=status_response.get('jobSummaryList', [])
-                for job_dict in status_list:
-                    query_dict.update({job_dict['jobId']: job_dict['status']})
-            for job in cls._job_id_map_status:
-                cls._job_id_map_status[job]= cls.map_aws_status_to_dpgen_status(query_dict.get(job, 'UNKNOWN'))
-            dlog.debug('20000: _query: %s, _map: %s' %(query_dict, cls._job_id_map_status))
-        if caller_instance:
-            return cls._job_id_map_status.get(caller_instance.job_id, JobStatus.unknown)
-        return cls._job_id_map_status
-
     @staticmethod
     def map_aws_status_to_dpgen_status(aws_status):
         map_dict = {'SUBMITTED': JobStatus.waiting,
@@ -56,8 +29,52 @@ class AWS(Batch):
                 'UNKNOWN': JobStatus.unknown}
         return map_dict.get(aws_status, JobStatus.unknown)
 
+    @classmethod
+    def AWS_check_status(cls, job_id=""):
+        """
+        to aviod query jobStatus too often, set a time interval
+        query_dict example:
+        {job_id: JobStatus}
+
+        {'40fb24b2-d0ca-4443-8e3a-c0906ea03622': <JobStatus.running: 3>,
+         '41bda50c-0a23-4372-806c-87d16a680d85': <JobStatus.waiting: 2>}
+           
+        """
+        query_dict ={}
+        if datetime.now().timestamp() > cls._query_next_allow_time:
+            cls._query_next_allow_time=datetime.now().timestamp()+cls._query_time_interval
+            for status in ['SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING', 'RUNNING','SUCCEEDED', 'FAILED']:
+                status_response = cls.batch.list_jobs(jobQueue=cls._jobQueue, jobStatus=status, maxResults=cls._query_max_results)
+                status_list=status_response.get('jobSummaryList', [])
+                for job_dict in status_list:
+                    query_dict.update({job_dict['jobId']: cls.map_aws_status_to_dpgen_status(job_dict['status'])})
+            for job in cls._job_id_map_status:
+                cls._job_id_map_status[job]=query_dict.get(job, JobStatus.unknown)
+            dlog.debug('20000: _query: %s, _map: %s' %(query_dict, cls._job_id_map_status))
+        dlog.debug('62000:job_id:%s, _query: %s, _map: %s' %(job_id, query_dict, cls._job_id_map_status))
+        if job_id:
+            return cls._job_id_map_status.get(job_id, query_dict.get(job_id,JobStatus.unknown))
+                    
+        return cls._job_id_map_status
+
     @property
     def job_id(self):
+        try:
+            self._job_id
+        except AttributeError:
+            if self.context.check_file_exists(self.job_id_name):
+                self._job_id = self.context.read_file(self.job_id_name)
+                response_list = self.__class__.batch.describe_jobs(jobs=[self._job_id]).get('jobs')
+                try:
+                    response = response_list[0]
+                    jobQueue = response['jobQueue']
+                except IndexError:
+                    pass
+                else:
+                    self.job_id = (response, jobQueue)
+                    return self._job_id
+            dlog.debug("50000, self._job_id:%s,_Queue:%s,_map:%s,"%(self._job_id, self.__class__._jobQueue, self.__class__._job_id_map_status  ))       
+            return ""
         return self._job_id
 
     @job_id.setter
@@ -66,11 +83,12 @@ class AWS(Batch):
         self._job_id = response['jobId']
         self._job_name = response['jobName']
         self.__class__._jobQueue = jobQueue
-        self.__class__._job_id_map_status[self._job_id] = JobStatus.waiting
+        self.__class__._job_id_map_status[self._job_id] = self.map_aws_status_to_dpgen_status(response.get('status', 'SUBMITTED'))
+        self.context.write_file(self.job_id_name, self._job_id)
         dlog.debug("15000, _job_id:%s, _job_name:%s, _map:%s, _Queue:%s" % (self._job_id, self._job_name, self.__class__._job_id_map_status, self.__class__._jobQueue))
 
     def check_status(self):
-        return self.__class__.AWS_check_status(caller_instance=self)
+        return self.__class__.AWS_check_status(job_id=self.job_id)
     
     def sub_script(self, job_dirs, cmd, args, res, outlog, errlog):
         """
@@ -100,7 +118,7 @@ class AWS(Batch):
             res.setdefault('jobQueue', 'deepmd_m5_v1_7')
         return res
     
-    def submit(self,
+    def do_submit(self,
             job_dirs,
             cmd,
             args = None,
@@ -119,6 +137,7 @@ class AWS(Batch):
         home-ec2-user-Ag_init-run_gen-iter_000000-01_model_devi-task_000_000048
         """
         jobName = os.path.join(self.context.remote_root,job_dirs.pop())[1:].replace('/','-').replace('.','_')
+        jobName += ("_" + str(self.context.job_uuid))
         response = self.__class__.batch.submit_job(jobName=jobName, 
                 jobQueue=res['jobQueue'], 
                 jobDefinition=res['jobDefinition'],
