b5c15d1bb969e053c613e7e9bfb34f3a1f947ebd
Waikit Chan <37867949+BaozCWJ@users.noreply.github.com>
2019-10-24 23:18:28 +0800

    Update util.py

--- dpgen/auto_test/lib/util.py
+++ dpgen/auto_test/lib/util.py
@@ -17,6 +17,7 @@ def voigt_to_stress(inpt) :
 def insert_data(task,task_type,username,file_name):
     assert task in ['eos','elastic','surf']
     assert task_type in ['vasp','deepmd']
+    #check the corresponding of expr_type and data_type
     url='http://115.27.161.2:5000/insert_test_data?username=%s&expr_type=%s&data_type=%s' % (username,task_type,task)
     res = requests.post(url, data=open(file_name).read())
     print('Successful upload!')
