2d3caff3fb819252f1d8126de2a97ca71305ca66
haidi <haidi@mail.ustc.edu.cn>
2020-03-10 15:09:10 +0800

    cvasp support for autotest

--- dpgen/__init__.py
+++ dpgen/__init__.py
@@ -8,11 +8,10 @@ NAME="dpgen"
 SHORT_CMD="dpgen"
 dlog = logging.getLogger(__name__)
 dlog.setLevel(logging.INFO)
-dlogf = logging.FileHandler(os.getcwd()+os.sep+SHORT_CMD+'.log')
-dlogf_formatter=logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
-#dlogf_formatter=logging.Formatter('%(asctime)s - %(name)s - [%(filename)s:%(funcName)s - %(lineno)d ] - %(levelname)s \n %(message)s')
-dlogf.setFormatter(dlogf_formatter)
-dlog.addHandler(dlogf)
+sh = logging.StreamHandler()
+formatter=logging.Formatter('%(asctime)s - %(name)s - [%(filename)s:%(funcName)s - %(lineno)d ] - %(levelname)s \n %(message)s')
+sh.setFormatter(formatter)
+dlog.addHandler(sh)
 
 __author__    = "Han Wang"
 __copyright__ = "Copyright 2019"
