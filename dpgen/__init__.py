20c94ceb2a2ff606ccaa8da0cf7603939d6f3310
haidi <haidi@mail.ustc.edu.cn>
2020-03-10 16:49:27 +0800

    recover __init__.py

--- dpgen/__init__.py
+++ dpgen/__init__.py
@@ -8,10 +8,11 @@ NAME="dpgen"
 SHORT_CMD="dpgen"
 dlog = logging.getLogger(__name__)
 dlog.setLevel(logging.INFO)
-sh = logging.StreamHandler()
-formatter=logging.Formatter('%(asctime)s - %(name)s - [%(filename)s:%(funcName)s - %(lineno)d ] - %(levelname)s \n %(message)s')
-sh.setFormatter(formatter)
-dlog.addHandler(sh)
+dlogf = logging.FileHandler(os.getcwd()+os.sep+SHORT_CMD+'.log')
+dlogf_formatter=logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
+#dlogf_formatter=logging.Formatter('%(asctime)s - %(name)s - [%(filename)s:%(funcName)s - %(lineno)d ] - %(levelname)s \n %(message)s')
+dlogf.setFormatter(dlogf_formatter)
+dlog.addHandler(dlogf)
 
 __author__    = "Han Wang"
 __copyright__ = "Copyright 2019"
