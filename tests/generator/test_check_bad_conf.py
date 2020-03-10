f611c31f0f0692e5f968c03d9aac63722a4ac6cc
Han Wang <wang_han@iapcm.ac.cn>
2020-03-10 08:55:28 +0800

    fix bug of test

--- tests/generator/test_check_bad_conf.py
+++ tests/generator/test_check_bad_conf.py
@@ -9,7 +9,8 @@ from .context import check_bad_conf
 
 class TestCheckBadConf(unittest.TestCase):
     def test_length_ratio(self):
-        conf_bad = os.path.join('check_bad_conf', 'bad.lammpstrj')
-        conf_good = os.path.join('check_bad_conf', 'good.lammpstrj')
+        dirname = os.path.dirname(__file__)
+        conf_bad = os.path.join(dirname, 'check_bad_conf', 'bad.lammpstrj')
+        conf_good = os.path.join(dirname, 'check_bad_conf', 'good.lammpstrj')
         self.assertTrue(check_bad_conf(conf_bad, 'length_ratio:5'))
         self.assertFalse(check_bad_conf(conf_good, 'length_ratio:5'))
