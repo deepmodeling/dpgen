51c30fb0945d66d63f0ea5a47214156c2f3ece2b
dingzhaohan <dingzhaohan@pku.edu.cn>
2020-04-17 14:45:30 +0800

    add DispatcherList

--- dpgen/dispatcher/DispatcherList.py
+++ dpgen/dispatcher/DispatcherList.py
@@ -1,11 +1,11 @@
 from dpgen.dispatcher.Dispatcher import Dispatcher, _split_tasks, JobRecord
 
 class Entity():
-    def __init__(self, ip, instance_id, job_handler):
+    def __init__(self, ip, instance_id, job_handler=None):
         self.ip = ip
         self.instance_id = instance_id
         self.job_handler = job_handler
-        
+
 class DispatcherList():
     def __init__(self, mdata_machine, mdata_resources, nchunks, cloud_resources=None):
         self.mdata_machine = mdata_machine
@@ -17,8 +17,8 @@ class DispatcherList():
                                      "entity": None} for ii in range(nchunks))
     # Base
     def init(self):
-        for dispatcher in self.dispatcher_list:
-            self.create()
+        for ii in range(self.nchunks):
+            self.create(ii)
 
     # Base
     def run_jobs(self,
@@ -59,27 +59,35 @@ class DispatcherList():
                     # to do 
                     # no jobs in queue, delete current machine
                     # else allocate current machine to unalloc dispatcher
+                    pass
                 elif dispatcher_status == "running":
                     pass
                 elif dispatcher_status == "unallocated":
                     # to do: if we can create machine, then make dispatcher
                     #        else pass
+                    pass
                 elif dispatcher_status == "terminated":
                     pass
 
     # Derivate
-    def check_restart(self):
-        '''use some methods to decide restart or not'''
-        pass
-
-    # Derivate
-    def create(self, num):
-        '''create machines
-           assignment the entities
-           make dispatchers
-           change status from unallocated to unsubmitted'''
-        pass
-
+    def create(self, ii):
+        '''if jr.json existed and job not finished, use jr.json to rebuild dispatcher
+           else create one machine, then make_dispatcher, change status from unallocated to unsubmitted'''
+        if not os.path.exists(os.path.join(os.path.abspath(work_path), "jr.%.06d.json" % ii)):
+            # to do: create machine, make dispatcher, change status from unallocated to unsubmitted
+            pass
+        else:
+            task_chunks = _split_tasks(tasks, group_size)
+            task_chunks_str = ['+'.join(ii) for ii in self.task_chunks]
+            task_hashes = [sha1(ii.encode('utf-8')).hexdigest() for ii in task_chunks_str]
+            job_record = JobRecord(work_path, task_chunks[ii], fname = "jr.%.06d.json" % ii)
+            cur_chunk = self.task_chunks[ii]
+            cur_hash = task_hashes[ii]
+            if not job_record.check_finished(cur_hash): 
+                with open(os.path.join(work_path, fn)) as fp:
+                    jr = json.load(fp)
+                    self.dispatcher_list[ii]["entity"] = Entity(jr[cur_hash]['context'][3], jr[cur_hash]['context'][4])
+                    self.make_dispatcher(ii)
     # Derivate
     def delete(self):
         '''delete one machine'''
@@ -111,14 +119,15 @@ class DispatcherList():
         pass
 
     # Base
-    def make_dispatcher(self, entity):
+    def make_dispatcher(self, ii):
         '''use entity to distinguish machine, for example if ip isn't None, means we can make_dispatcher
            change status from unallocated to unsubmitted'''
-        if entity["ip"]:
+        entity = self.dispatcher_list[ii]["entity"]
+        if entity.ip:
             profile = self.mdata_machine.copy()
-            profile['hostname'] = entity["ip"]
-            profile['instance_id'] = entity["instance_id"]
-            dispatcher = Dispatcher(profile, context_type='ssh', batch_type='shell', job_record='jr.%.06d.json' % ii)
+            profile['hostname'] = entity.ip
+            profile['instance_id'] = entity.instance_id
+            self.dispatcher_list[ii]["dispatcher"] = Dispatcher(profile, context_type='ssh', batch_type='shell', job_record='jr.%.06d.json' % ii)
             self.dispatcher_list[ii]["dispatcher_status"] = "unsubmitted"
 
     # Base
