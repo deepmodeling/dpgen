c5952833f2f322c82c3d7fe628e017aecea50a8e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-07-19 14:28:24 -0400

    fix ImportError

--- dpgen/generator/styles/__init__.py
+++ dpgen/generator/styles/__init__.py
@@ -3,7 +3,10 @@ from pathlib import Path
 try:
     from importlib import metadata
 except ImportError: # for Python<3.8
-    import importlib_metadata as metadata
+    try:
+        import importlib_metadata as metadata
+    except ImportError:
+        metadata = None
 
 PACKAGE_BASE = "dpgen.generator.styles"
 NOT_LOADABLE = ("__init__.py",)
@@ -14,6 +17,7 @@ for module_file in Path(__file__).parent.glob("*.py"):
         importlib.import_module(module_name, PACKAGE_BASE)
 
 # https://setuptools.readthedocs.io/en/latest/userguide/entry_point.html
-eps = metadata.entry_points().get('dpdata.styles', [])
-for ep in eps:
-    plugin = ep.load()
+if metadata:
+    eps = metadata.entry_points().get('dpdata.styles', [])
+    for ep in eps:
+        plugin = ep.load()
