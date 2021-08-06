05e1dafe13bd14f11f22b03a0b1b55742c7296fd
Han Wang <amcadmus@gmail.com>
2021-08-07 00:18:23 +0800

    Update test_convert_mdata.py

--- tests/tools/test_convert_mdata.py
+++ tests/tools/test_convert_mdata.py
@@ -5,11 +5,10 @@ test_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.join(test_dir, '..'))
 __package__ = 'tools'
 from dpgen.remote.decide_machine import convert_mdata
-from .context import switch_to_file_dir
+from .context import setUpModule
 machine_file = 'machine_fp_single.json'
 class TestConvertMdata(unittest.TestCase):
     def test_convert_mdata (self):
-        switch_to_file_dir()
         mdata = json.load(open(machine_file))
         mdata = convert_mdata(mdata, ["fp"])
         self.assertEqual(mdata["fp_command"], "vasp_std")
