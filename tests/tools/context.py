ef6b44edb24feb410d6d7078d62628c3754ab74b
Han Wang <amcadmus@gmail.com>
2021-08-07 00:04:09 +0800

    Update context.py

--- tests/tools/context.py
+++ tests/tools/context.py
@@ -2,7 +2,7 @@ import sys,os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dpgen.tools.run_report import *
-machine_file = 'machine_fp_single.json'
+
 def my_file_cmp(test, f0, f1):
     with open(f0) as fp0 :
         with open(f1) as fp1:
