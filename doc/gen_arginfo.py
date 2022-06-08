809867b1912718a396de467f786d6a53ee51f245
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-06-07 23:16:37 -0400

    generate run arg doc from scripts
    This commit generates run argument documentations from scripts at runtime, so we can work on the script to improve the docs.

--- doc/gen_arginfo.py
+++ doc/gen_arginfo.py
@@ -1,5 +1,12 @@
-from dpgen.generator.arginfo import run_mdata_arginfo
+from dpgen.generator.arginfo import (
+    run_mdata_arginfo,
+    run_jdata_arginfo,
+)
 
-run_mdata_doc = run_mdata_arginfo().gen_doc()
+run_mdata_doc = run_mdata_arginfo().gen_doc(make_anchor=True)
 with open('run-mdata-auto.rst', 'w') as f:
     f.write(run_mdata_doc)
+
+run_jdata_doc = run_jdata_arginfo().gen_doc(make_anchor=True)
+with open('run-jdata-auto.rst', 'w') as f:
+    f.write(run_jdata_doc)
