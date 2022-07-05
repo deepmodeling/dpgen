f6a0e3760929c660dd422946c5aa7713ba2cba4a
HuangJiameng <105633685+HuangJiameng@users.noreply.github.com>
2022-07-06 05:52:39 +0800

    Update simplify.py
    for sys_entire, set labeled=False

--- dpgen/simplify/simplify.py
+++ dpgen/simplify/simplify.py
@@ -217,7 +217,7 @@ def post_model_devi(iter_index, jdata, mdata):
     sys_candinate = dpdata.MultiSystems()
     sys_failed = dpdata.MultiSystems()
 
-    sys_entire = dpdata.MultiSystems().from_deepmd_npy(os.path.join(work_path, rest_data_name + ".old"))
+    sys_entire = dpdata.MultiSystems().from_deepmd_npy(os.path.join(work_path, rest_data_name + ".old"), labeled=False)
 
     detail_file_name = detail_file_name_prefix
     with open(os.path.join(work_path, detail_file_name)) as f:
