0065cdb9c8618021b43da5e67286a6810851ec59
HuangJiameng <105633685+HuangJiameng@users.noreply.github.com>
2022-08-24 16:12:31 +0800

    update init_surf with dpgen.generator.lib.utils.symlink_user_forward_files

--- dpgen/data/surf.py
+++ dpgen/data/surf.py
@@ -13,6 +13,7 @@ from dpgen import dlog
 from dpgen import ROOT_PATH
 from dpgen.remote.decide_machine import  convert_mdata
 from dpgen.dispatcher.Dispatcher import make_submission_compat
+from dpgen.generator.lib.utils import symlink_user_forward_files
 #-----PMG---------
 from pymatgen.io.vasp import Poscar
 from pymatgen.core import Structure, Element
@@ -548,11 +549,13 @@ def run_vasp_relax(jdata, mdata):
     forward_files = ["POSCAR", "INCAR", "POTCAR"]
     backward_files = ["OUTCAR","CONTCAR"]
     forward_common_files = []
-    user_forward_files = mdata['fp'].get('forward_files', []) 
-    for ii in user_forward_files :
-        if not os.path.exists(ii) :
-            raise FileNotFoundError('Forward files not found. Please check the path set in "forward_files". ')
-        forward_files.append(os.path.basename(ii))
+    work_path_list = glob.glob(os.path.join(work_dir, "surf-*"))
+    task_format = {"fp" : "sys-*"}
+    for work_path in work_path_list :
+        symlink_user_forward_files(mdata=mdata, task_type="fp", work_path=work_path, task_format=task_format)
+    user_forward_files = mdata.get("fp" + "_user_forward_files", [])
+    forward_files += [os.path.basename(file) for file in user_forward_files]
+    backward_files += mdata.get("fp" + "_user_backward_files", [])
     #if 'cvasp' in mdata['fp_resources']:
     #    if mdata['fp_resources']['cvasp']:
     #        forward_common_files=['cvasp.py']
@@ -567,9 +570,6 @@ def run_vasp_relax(jdata, mdata):
     for ii in relax_tasks : 
         if not _vasp_check_fin(ii):
             relax_run_tasks.append(ii)
-            for jj in user_forward_files: 
-                os.symlink(os.path.abspath(jj), os.path.abspath(
-                    os.path.join(ii, os.path.basename(jj))))
     run_tasks = [ii.replace(work_dir+"/", "") for ii in relax_run_tasks]
 
     #dlog.info(run_tasks)
