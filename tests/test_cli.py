4002d09256a7494da209b2b70370afa68496e1f6
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-10-16 18:59:57 -0400

    bugfix

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,6 @@ import subprocess as sp
 
 class TestCLI(unittest.TestCase):
     def test_cli(self):
-        sp.check_output([sys.executable, "-m", "dpgen", "-h"])
+        sp.check_output(["dpgen", "-h"])
         for subcommand in ('run', 'simplify', 'init_surf', 'init_bulk', 'init_reaction', 'autotest'):
-            sp.check_output([sys.executable, "-m", "dpgen", subcommand, "-h"])
+            sp.check_output(["dpgen", subcommand, "-h"])
