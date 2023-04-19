40c94e0fa16545d8b558d970da5a11a0f40478d0
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-04-19 19:51:42 -0400

    change size and apply dark mode for the logo
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- doc/conf.py
+++ doc/conf.py
@@ -75,13 +75,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_logo = "logo.svg"
+html_logo = "_static/logo.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-# html_css_files = ['css/custom.css']
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
 
 autodoc_default_flags = ["members"]
 autosummary_generate = True
