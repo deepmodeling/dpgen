0aec52e97d90d03c7edefb336e4fd79c2f5ecd83
Peng Xingliang <91927439+pxlxingliang@users.noreply.github.com>
2023-05-16 08:53:14 +0800

    fix(abacus): put pp/orb/dpks files to folder pporb in 02fp. (#1214)
    #1192
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dpgen/generator/lib/abacus_scf.py
+++ dpgen/generator/lib/abacus_scf.py
@@ -25,8 +25,9 @@ def make_abacus_scf_kpt(fp_params):
     return ret
 
 
-def make_abacus_scf_input(fp_params):
+def make_abacus_scf_input(fp_params, extra_file_path=""):
     # Make INPUT file for abacus pw scf calculation.
+    # put extra files (such as: deepks_model) to extra_file_path folder
     ret = "INPUT_PARAMETERS\n"
     ret += "calculation scf\n"
     for key in fp_params:
@@ -181,7 +182,9 @@ def make_abacus_scf_input(fp_params):
             ), "'deepks_scf' should be either 0 or 1."
             ret += "deepks_scf %d\n" % fp_params["deepks_scf"]
         elif key == "deepks_model":
-            ret += "deepks_model %s\n" % fp_params["deepks_model"]
+            ret += "deepks_model %s\n" % os.path.join(
+                extra_file_path, os.path.split(fp_params["deepks_model"])[1]
+            )
         elif key[0] == "_":
             pass
         elif key == "calculation":
@@ -198,6 +201,7 @@ def make_abacus_scf_stru(
     fp_dpks_descriptor=None,
     fp_params=None,
     type_map=None,
+    pporb="",  # pull all pp orb dpks files to pporb folder
 ):
     atom_names = sys_data["atom_names"]
     atom_numbs = sys_data["atom_numbs"]
@@ -217,12 +221,17 @@ def make_abacus_scf_stru(
         )
         idx = type_map.index(atom_names[iatom])
         if "atom_masses" not in sys_data:
-            ret += atom_names[iatom] + " 1.00 " + fp_pp_files[idx] + "\n"
+            ret += (
+                atom_names[iatom]
+                + " 1.00 "
+                + os.path.join(pporb, fp_pp_files[idx])
+                + "\n"
+            )
         else:
             ret += (
                 atom_names[iatom]
                 + " %.3f " % sys_data["atom_masses"][iatom]
-                + fp_pp_files[idx]
+                + os.path.join(pporb, fp_pp_files[idx])
                 + "\n"
             )
 
@@ -267,11 +276,11 @@ def make_abacus_scf_stru(
         assert len(fp_orb_files) == len(type_map)
         for iatom in range(len(atom_names)):
             idx = type_map.index(atom_names[iatom])
-            ret += fp_orb_files[idx] + "\n"
+            ret += os.path.join(pporb, fp_orb_files[idx]) + "\n"
 
     if fp_dpks_descriptor is not None:
         ret += "\nNUMERICAL_DESCRIPTOR\n"
-        ret += "%s\n" % fp_dpks_descriptor
+        ret += os.path.join(pporb, fp_dpks_descriptor) + "\n"
 
     return ret
 
