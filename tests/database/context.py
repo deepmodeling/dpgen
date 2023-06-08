77487e70e5c064b82feb850e9ef0dfb520f917df
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-06-08 16:05:51 -0400

    fix contexts
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/database/context.py
+++ tests/database/context.py
@@ -2,6 +2,10 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+import dpgen  # noqa: F401
+from dpgen.database.entry import Entry  # noqa: F401
+from dpgen.database.run import parsing_vasp  # noqa: F401
+from dpgen.database.vasp import DPPotcar, VaspInput  # noqa: F401
 
 
 def setUpModule():
