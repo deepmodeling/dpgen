5972106ad31534f7bc8ceda390daba2f77bc77ee
Levi Zhou <31941107+ZhouXY-PKU@users.noreply.github.com>
2023-08-05 11:54:11 +0800

    Update VASP.py
    Signed-off-by: Levi Zhou  <31941107+ZhouXY-PKU@users.noreply.github.com>

--- dpgen/auto_test/VASP.py
+++ dpgen/auto_test/VASP.py
@@ -190,7 +190,7 @@ class VASP(Task):
             os.symlink("../INCAR", "INCAR")
         os.chdir(cwd)
         ret = vasp.make_kspacing_kpoints(self.path_to_poscar, kspacing, kgamma)
-        pymgv == "old":
+        pymgv = "old"
         try:
             kp = Kpoints.from_string(ret)
         except:
