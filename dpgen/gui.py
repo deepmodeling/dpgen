0851f48d5cb1271c5bc1e4a5d1bf2f97c253d1ab
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-07 00:14:36 -0400

    fix start_dpgui
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dpgen/gui.py
+++ dpgen/gui.py
@@ -1,19 +1,15 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 """DP-GUI entrypoint."""
+import argparse
 
 
-def start_dpgui(*, port: int, bind_all: bool, **kwargs):
+def start_dpgui(args: argparse.Namespace):
     """Host DP-GUI server.
 
     Parameters
     ----------
-    port : int
-        The port to serve DP-GUI on.
-    bind_all : bool
-        Serve on all public interfaces. This will expose your DP-GUI instance
-        to the network on both IPv4 and IPv6 (where available).
-    **kwargs
-        additional arguments
+    args : argparse.Namespace
+        Arguments from argparse.
 
     Raises
     ------
@@ -28,4 +24,4 @@ def start_dpgui(*, port: int, bind_all: bool, **kwargs):
         raise ModuleNotFoundError(
             "To use DP-GUI, please install the dpgui package:\npip install dpgui"
         ) from e
-    start_dpgui(port=port, bind_all=bind_all)
+    start_dpgui(port=args.port, bind_all=args.bind_all)
