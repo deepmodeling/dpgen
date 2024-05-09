f3f49d3d3f1d52bbadd0628276cab24871f8fdaa
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-05-09 10:12:26 +0900

    remove gpaw

--- dpgen/generator/run.py
+++ dpgen/generator/run.py
@@ -3762,33 +3762,6 @@ def make_fp_custom(iter_index, jdata):
             system.to(input_fmt, input_fn)
 
 
-def make_fp_gpaw(iter_index, jdata):
-    """Make input file for customized FP style.
-
-    Convert the POSCAR file to ase_traj format (no need).
-
-    Parameters
-    ----------
-    iter_index : int
-        iter index
-    jdata : dict
-        Run parameters.
-    """
-    ## create symbolic link of the gpaw input file in the task directory
-    work_path = os.path.join(make_iter_name(iter_index), fp_name)
-    fp_tasks = glob.glob(os.path.join(work_path, "task.*"))
-    gpaw_runfile = jdata["fp_gpaw_runfile"]
-    gpaw_runfile_source = Path(gpaw_runfile).resolve()
-    assert os.path.exists(
-        gpaw_runfile_source
-    ), f"Can not find gpaw runfile {gpaw_runfile_source}"
-    for ii in fp_tasks:
-        with set_directory(
-            Path(ii)
-        ):  # create file `gpaw_runfile` in the current directory and symlink it to the source file
-            Path(gpaw_runfile).symlink_to(gpaw_runfile_source)
-
-
 def make_fp(iter_index, jdata, mdata):
     """Select the candidate strutures and make the input file of FP calculation.
 
@@ -3838,8 +3811,6 @@ def make_fp_calculation(iter_index, jdata, mdata):
         make_fp_amber_diff(iter_index, jdata)
     elif fp_style == "custom":
         make_fp_custom(iter_index, jdata)
-    elif fp_style == "gpaw":
-        make_fp_gpaw(iter_index, jdata)
     else:
         raise RuntimeError("unsupported fp style")
     # Copy user defined forward_files
@@ -4161,19 +4132,6 @@ def run_fp(iter_index, jdata, mdata):
             backward_files,
             None,
             log_file="output",
-        )
-    elif fp_style == "gpaw":
-        gpaw_runfile = jdata["fp_gpaw_runfile"]
-        forward_files = ["POSCAR"] + [gpaw_runfile]
-        backward_files = ["conf_ase.traj", "calc.txt", "run.log"]
-        run_fp_inner(
-            iter_index,
-            jdata,
-            mdata,
-            forward_files,
-            backward_files,
-            None,
-            log_file="run.log",
         )
     else:
         raise RuntimeError("unsupported fp style")
@@ -4676,50 +4634,6 @@ def post_fp_custom(iter_index, jdata):
         all_sys.to_deepmd_npy(sys_data_path, set_size=len(sys_output), prec=np.float64)
 
 
-def post_fp_gpaw(iter_index, jdata):
-    """Post fp for custom fp. Collect data from user-defined `output_fn`.
-
-    Parameters
-    ----------
-    iter_index : int
-        The index of the current iteration.
-    jdata : dict
-        The parameter data.
-    """
-    model_devi_jobs = jdata["model_devi_jobs"]
-    assert iter_index < len(model_devi_jobs)
-
-    iter_name = make_iter_name(iter_index)
-    work_path = os.path.join(iter_name, fp_name)
-    fp_tasks = glob.glob(os.path.join(work_path, "task.*"))
-    fp_tasks.sort()
-    if len(fp_tasks) == 0:
-        return
-
-    system_index = []
-    for ii in fp_tasks:
-        system_index.append(os.path.basename(ii).split(".")[1])
-    system_index.sort()
-    set_tmp = set(system_index)
-    system_index = list(set_tmp)
-    system_index.sort()
-
-    output_fn = "conf_ase.traj"
-    output_fmt = "ase/traj"
-
-    for ss in system_index:
-        sys_output = glob.glob(os.path.join(work_path, "task.%s.*" % ss))
-        sys_output.sort()
-        all_sys = dpdata.MultiSystems(type_map=jdata["type_map"])
-        for oo in sys_output:
-            if os.path.exists(os.path.join(oo, output_fn)):
-                sys = dpdata.LabeledSystem(os.path.join(oo, output_fn), fmt=output_fmt)
-                all_sys.append(sys)
-        sys_data_path = os.path.join(work_path, "data.%s" % ss)
-        all_sys.to_deepmd_raw(sys_data_path)
-        all_sys.to_deepmd_npy(sys_data_path, set_size=len(sys_output), prec=np.float64)
-
-
 def post_fp(iter_index, jdata):
     fp_style = jdata["fp_style"]
     if fp_style == "vasp":
@@ -4740,8 +4654,6 @@ def post_fp(iter_index, jdata):
         post_fp_amber_diff(iter_index, jdata)
     elif fp_style == "custom":
         post_fp_custom(iter_index, jdata)
-    elif fp_style == "gpaw":
-        post_fp_gpaw(iter_index, jdata)
     else:
         raise RuntimeError("unsupported fp style")
     post_fp_check_fail(iter_index, jdata)
