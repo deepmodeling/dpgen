deepmodeling/dpdata

commit 482775f19e11ae77e7ee9835ee23068da11c52cb

Fix: qe/pw/scf unit conversion is not consistent with dpdata (#725)
<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

- **New Features**
- Introduced a configuration file for sodium SCF calculations, enhancing
quantum mechanical simulations.
- Added a new test class to verify properties of sodium systems,
improving test coverage.

- **Bug Fixes**
- Improved logic for block extraction and stress block handling to
enhance robustness.

- **Tests**
- Updated energy validation method to use relative comparisons for more
accurate results.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

---------

Signed-off-by: Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

dpdata/qe/scf.py
@@ -7,9 +7,13 @@ import numpy as np
 
 from dpdata.utils import open_file
 
-ry2ev = 13.605693009
-bohr2ang = 0.52917721067
-kbar2evperang3 = 1e3 / 1.602176621e6
+from .traj import (
+    kbar2evperang3,
+    ry2ev,
+)
+from .traj import (
+    length_convert as bohr2ang,
+)
 
 _QE_BLOCK_KEYWORDS = [
     "ATOMIC_SPECIES",

tests/test_qe_pw_scf.py
@@ -161,11 +161,11 @@ class TestPWSCFSinglePointEnergy:
 
     def test_energy(self):
         ref_energy = -219.74425946528794
-        self.assertAlmostEqual(self.system_ch4.data["energies"][0], ref_energy)
+        self.assertAlmostEqual(self.system_ch4.data["energies"][0] / ref_energy, 1.0)
         ref_energy = -30007.651851226798
-        self.assertAlmostEqual(self.system_h2o.data["energies"][0], ref_energy)
+        self.assertAlmostEqual(self.system_h2o.data["energies"][0] / ref_energy, 1.0)
         ref_energy = -219.7153691367526562
-        self.assertAlmostEqual(self.system_ch4_2.data["energies"][0], ref_energy)
+        self.assertAlmostEqual(self.system_ch4_2.data["energies"][0] / ref_energy, 1.0)
 
 
 class TestPWSCFLabeledOutput(unittest.TestCase, TestPWSCFSinglePointEnergy):

tests/test_qe_pw_scf_energy_bug.py
@@ -8,7 +8,7 @@ from context import dpdata
 class TestPWSCFSinglePointEnergy:
     def test_energy(self):
         ref_energy = -296.08379065679094669
-        self.assertAlmostEqual(self.system_al.data["energies"][0], ref_energy)
+        self.assertAlmostEqual(self.system_al.data["energies"][0] / ref_energy, 1.0)
 
 
 class TestPWSCFLabeledOutput(unittest.TestCase, TestPWSCFSinglePointEnergy):

deepmodeling/dpgen

commit 453b49fe84e1bcc817bc2f51fd7a328296879ff0

fix: compare pwscf energy by relative error (#1643)
ut failure caused by https://github.com/deepmodeling/dpdata/pull/725

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->

## Summary by CodeRabbit

- **Bug Fixes**
- Improved handling of edge cases in coordinate and energy tests,
enhancing test robustness for zero values.
- Added conditional checks to ensure accurate comparisons in energy
calculations.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

---------

Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

tests/generator/comp_sys.py
@@ -86,6 +86,9 @@ class CompSys:
         tmp_cell = self.system_1.data["cells"]
         tmp_cell = np.reshape(tmp_cell, [-1, 3])
         tmp_cell_norm = np.reshape(np.linalg.norm(tmp_cell, axis=1), [-1, 3])
+        if np.max(np.abs(tmp_cell_norm)) < 1e-12:
+            # zero cell, no pbc case, set to [1., 1., 1.]
+            tmp_cell_norm = np.ones(tmp_cell_norm.shape)
         for ff in range(self.system_1.get_nframes()):
             for ii in range(sum(self.system_1.data["atom_numbs"])):
                 for jj in range(3):
@@ -103,12 +106,21 @@ class CompLabeledSys(CompSys):
     def test_energy(self):
         self.assertEqual(self.system_1.get_nframes(), self.system_2.get_nframes())
         for ff in range(self.system_1.get_nframes()):
-            self.assertAlmostEqual(
-                self.system_1.data["energies"][ff],
-                self.system_2.data["energies"][ff],
-                places=self.e_places,
-                msg="energies[%d] failed" % (ff),
-            )
+            if abs(self.system_2.data["energies"][ff]) < 1e-12:
+                self.assertAlmostEqual(
+                    self.system_1.data["energies"][ff],
+                    self.system_2.data["energies"][ff],
+                    places=self.e_places,
+                    msg="energies[%d] failed" % (ff),
+                )
+            else:
+                self.assertAlmostEqual(
+                    self.system_1.data["energies"][ff]
+                    / self.system_2.data["energies"][ff],
+                    1.0,
+                    places=self.e_places,
+                    msg="energies[%d] failed" % (ff),
+                )
 
     def test_force(self):
         self.assertEqual(self.system_1.get_nframes(), self.system_2.get_nframes())