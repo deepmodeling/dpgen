79b06213061ce0c0039886eb35f9704f5555a71d
Scott Wu <71915685+scott-5@users.noreply.github.com>
2024-09-26 11:04:16 +0800

    make the screen output complete
    Signed-off-by: Scott Wu <71915685+scott-5@users.noreply.github.com>

--- dpgen/__init__.py
+++ dpgen/__init__.py
@@ -5,13 +5,13 @@ ROOT_PATH = __path__[0]
 NAME = "dpgen"
 SHORT_CMD = "dpgen"
 dlog = logging.getLogger(__name__)
-dlog.propagate = False
 dlog.setLevel(logging.INFO)
 dlogf = logging.FileHandler(os.getcwd() + os.sep + SHORT_CMD + ".log", delay=True)
 dlogf_formatter = logging.Formatter("%(asctime)s - %(levelname)s : %(message)s")
 # dlogf_formatter=logging.Formatter('%(asctime)s - %(name)s - [%(filename)s:%(funcName)s - %(lineno)d ] - %(levelname)s \n %(message)s')
 dlogf.setFormatter(dlogf_formatter)
 dlog.addHandler(dlogf)
+logging.addHandler(logging.StreamHandler())
 
 __author__ = "Han Wang"
 __copyright__ = "Copyright 2019"
